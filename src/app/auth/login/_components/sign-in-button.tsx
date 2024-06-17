6d4bd84c9c17e5778d4301446e0ddc2ca34c2822
Sergipe <71034184+sergipe085@users.noreply.github.com>
2024-06-17 02:08:50 -0300

    fix login redirect error

--- src/app/auth/login/_components/sign-in-button.tsx
+++ src/app/auth/login/_components/sign-in-button.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { Github } from "lucide-react";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
@@ -8,7 +9,7 @@ export function SignInButton() {
 
     return (
         <button onClick={() => signIn("github", {
-            callbackUrl: callback as string
+            callbackUrl: callback ? callback as string : DEFAULT_LOGIN_REDIRECT
         })} className="btn w-full btn-outline btn-neutral"><Github/></button>
     )
 }
